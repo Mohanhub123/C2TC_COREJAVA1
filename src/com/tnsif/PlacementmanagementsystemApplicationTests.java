78e1b8b2970ac3f7fd651ed4f7bb1c8d920f2498
Mohanhub123 <mohanrajkmohan1@gmail.com>
2025-02-17 12:19:14 +0700

    Add files via upload

---
+++ src/com/tnsif/PlacementmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.placementmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
